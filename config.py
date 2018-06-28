ef957e237c3ece1dfa0811ec55dd5261dd7a9457
Lasagna of Garfield <pattyjula@gmail.com>
2018-06-27 18:49:54 -0700

    new

---
+++ config.py
@@ -0,0 +1,5 @@
+# Twitter API Keys
+consumer_key = "sNxmEC5Db9yTn3TkR35Oehqca"
+consumer_secret = "yyiLUtGlqJBOwjaKg4W9PndN4v1wJDOpwJNpQpU5q6GY1pq6Nh"
+access_token = "545606176-39NTtKuXx6UavOYWMNcb3hknCkoA5ZZSSdepY9T3"
+access_token_secret = "vJGB1AGMKTAecr5VZYJtcqSpzDTtHp3IoLakJnB5mxJ6h"
